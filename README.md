4e06deeb06b02b5212639755f97009287d295373
ljy <lianjy357@qq.com>
2019-10-29 17:34:39 +0800

    与数据库连接

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # OK_Family_back
+
+# python install
+pip install
+
+records
+mysqlclient
+pymysql
+
+# configure
+需要在根目录新建一个文件`setting.json`
+将必要的数据填入其中
+```json
+{
+    "DB": {
+        "DatabaseType": "mysql",
+        "API": "pymysql",
+        "Username": "root",
+        "Password": "",
+        "IP": "",
+        "Port": "",
+        "DatabaseName": ""
+    }
+}
+```
+## 名词解析
+DB：数据库配置
+
+- DatabaseType：数据库类型
+- API：API
+- Username：数据库账号
+- Password：数据库密码
+- IP：地址
+- Port：端口
+- DatabaseName：数据库名
---
+++ database/OK_mysql.py
@@ -0,0 +1,23 @@
+import records
+import json
+
+# 获取配置信息
+with open("setting.json",'r', encoding='utf-8') as load_f:
+    load_dict = json.load(load_f)
+    
+
+# 数据库连接
+DBsetting = load_dict['DB']
+db = records.Database('{0}+{1}://{2}:{3}@{4}:{5}/{6}'\
+  .format(DBsetting['DatabaseType'], DBsetting['API'], DBsetting['Username'], DBsetting['Password'], DBsetting['IP'], DBsetting['Port'], DBsetting['DatabaseName']))
+
+# 创建表的sql
+create_sql = """CREATE TABLE IF NOT EXISTS user_deo(
+  id int(11) PRIMARY KEY AUTO_INCREMENT,
+  name varchar(20),
+  age int
+)"""
+print('创建成功')
+
+## 执行
+db.query(create_sql)
