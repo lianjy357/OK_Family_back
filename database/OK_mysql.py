de33c571b7b26112bb04aab274134ca1a01231ed
lianjy <lianjy357@qq.com>
2019-11-04 00:09:29 +0800

    处理了数据库操作的布局

--- database/OK_mysql.py
+++ database/OK_mysql.py
@@ -12,12 +12,16 @@ db = records.Database('{0}+{1}://{2}:{3}@{4}:{5}/{6}'\
   .format(DBsetting['DatabaseType'], DBsetting['API'], DBsetting['Username'], DBsetting['Password'], DBsetting['IP'], DBsetting['Port'], DBsetting['DatabaseName']))
 
 # 创建表的sql
-create_sql = """CREATE TABLE IF NOT EXISTS user_deo(
-  id int(11) PRIMARY KEY AUTO_INCREMENT,
-  name varchar(20),
-  age int
-)"""
+# create_sql = """CREATE TABLE IF NOT EXISTS user_deo(
+#   id int(11) PRIMARY KEY AUTO_INCREMENT,
+#   name varchar(20),
+#   age int
+# )"""
+user = [{"username": "yuze5", "password": 20, "nickname": "sally"},{"username": "yuze5", "password": 20, "nickname": "sally"}]
+# print('INSERT INTO sys_userInfo(username,password,nickname) values (:username, :password, :nickname)', **user)
+db.bulk_query('INSERT INTO sys_userInfo(username,password,nickname) values (:username, :password, :nickname)', user)
+
 print('创建成功')
 
 ## 执行
-db.query(create_sql)
+# db.query(create_sql)
